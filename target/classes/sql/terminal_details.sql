cef435912e2ec6e3e95a0789b7dcf59a808c07a4
RiteshParkhad <ritesh@relfor.com>
2026-07-07 18:49:59 +0530

    Add .gitignore and untrack compiled artifacts and keystore

--- target/classes/sql/terminal_details.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE pcs_personnel_management.terminal_details (
-  `id` bigint NOT NULL,
-  `store_details_id` bigint DEFAULT NULL,
-  `terminal_serial_number` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
